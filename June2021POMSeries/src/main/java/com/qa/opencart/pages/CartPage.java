0e542424885029d4b3c3557d5863d9af7d232337
Nadia Hussain <huss333513@exactsoftware.com>
2021-09-26 00:49:00 +0800

    code review comments fixed

--- June2021POMSeries/src/main/java/com/qa/opencart/pages/CartPage.java
+++ June2021POMSeries/src/main/java/com/qa/opencart/pages/CartPage.java
@@ -5,4 +5,8 @@ import org.openqa.selenium.By;
 public class CartPage {
 
 	By cart = By.linkText("cart");
+	
+	public void addToCart() {
+		System.out.println("add the product to the cart ");
+	}
 }

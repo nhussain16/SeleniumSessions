3f23529a64522abb44435160f4d9f06643b54d25
Nadia Hussain <huss333513@exactsoftware.com>
2021-09-25 23:31:19 +0800

    cart feature added

---
+++ June2021POMSeries/src/test/java/com/qa/opencart/tests/CartTest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	@Test
+	public void testCart() {
+		Assert.assertEquals(true, true);
+	}
+}
